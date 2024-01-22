124ac592580527292f0d14ac1860dd7d48ed7e14
Bugra <seyitbugraerden@gmail.com>
2024-01-22 12:24:32 +0300

    Axios get-post & firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TaskAdd from './components/TaskAdd';
 import Tasks from './components/Tasks';
+import axios from 'axios'
 import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const addTask = (newTask) => {
-    setTasks([...tasks, newTask]);
-  };
+  useEffect(() => {
+    axios.get('https://to-do-project-46be6-default-rtdb.firebaseio.com/task.json')
+        .then(response => {
+            const tasksArray = Object.values(response.data);
+            setTasks(tasksArray.map(task => task.newTask));
+        })
+}, []);
+
+
+
+
+const addTask = (newTask) => {
+  axios.post("https://to-do-project-46be6-default-rtdb.firebaseio.com/task.json", { newTask })
+    .then(response => {
+      setTasks([...tasks, newTask]);
+    })
+    .catch(error => console.error('Görev ekleme hatası:', error));
+};
+ 
 
   const deleteItem = (index) => {
     const updatedTasks = [...tasks];
